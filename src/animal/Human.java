598117605779ca43c8b031d25fadb7b788f5c8e9
OYAWAMA <fluting.dalton_0m@icloud.com>
2023-02-15 00:04:50 +0900

    課題提出

---
+++ src/animal/Human.java
@@ -0,0 +1,29 @@
+package animal;
+
+public class Human extends Animal implements Thinkable{
+    private String hobby; // 趣味
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // 引数なしコンストラクタ
+    public Human() {
+    }
+
+    // 引数ありコンストラクタ
+    public Human(String name, int age, String hobby) {
+        super(name, age); // スーパークラスのコンストラクタを呼び出し
+        this.hobby = hobby;
+
+    }
+
+    @Override
+    public void think() {
+        System.out.println("私は"+hobby+"について考えています。");
+    }
+}
